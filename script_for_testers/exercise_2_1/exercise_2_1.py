36664a7cd75cd5cef90e13d35e2f3557e2de614b
dev-tanvir <tanvirfaisaldev@gmail.com>
2019-06-03 00:27:07 +0600

    added google chart for reporting on test results

--- script_for_testers/exercise_2_1/exercise_2_1.py
+++ script_for_testers/exercise_2_1/exercise_2_1.py
@@ -1,4 +1,5 @@
 import csv
+from string import Template
 
 timing_data = []    # we want list of lists 
                     # column_chart = [['Test Name','Diff from Avg']]
@@ -29,3 +30,38 @@ for row in timing_data[1:]:
 # print("---------------------------")
 # print(table_data)
 
+html_string = Template('''
+<html>
+<head>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  function drawChart () {
+  var data = google.visualization.arrayToDataTable([
+       $labels,
+       $data
+      ],
+      false); // 'false' means that the first row contains labels, not data.
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      chart.draw(data);
+
+}
+</script>
+</head>
+<body>
+    <div id="chart_div" style="width:1000; height:500"></div>
+</body>
+</html>
+''')
+
+chart_data_str = ''
+
+for row in column_chart[1:]:
+    chart_data_str += '%s,\n'%row
+
+completed_html = html_string.substitute(labels=column_chart[0],data=chart_data_str)
+
+with open('column_chart.html','w') as output_file:
+    output_file.write(completed_html)
